c31c2874355f931a4579b6c58e7d62638c1638a3
Tyrone Gaylord <tyronegaylord@Tyrones-MacBook-Pro.local>
2022-07-19 14:12:11 -0500

    adding mono repo

---
+++ packages/simple-express-app/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get("/data", (req, res) => {
+  res.json({ foo: "bar" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
