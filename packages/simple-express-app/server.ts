60350f199341171f2f0ac2099f63166d847e9951
Tyrone Gaylord <tyronegaylord@Tyrones-MacBook-Pro.local>
2022-07-19 16:10:21 -0500

    change repo setup

--- packages/simple-express-app/server.ts
+++ packages/simple-express-app/server.ts
@@ -2,13 +2,14 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.get("/data", (req, res) => {
+  res.setHeader('access-control-allow-origin', '*');
   res.json({ foo: "bar" });
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
